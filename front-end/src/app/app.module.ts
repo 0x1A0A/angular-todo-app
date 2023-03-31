9ef21f2fba5c7ce8ba9cf136b209f657dbeca724
Few <odds@Few.local>
2023-03-31 11:27:33 +0700

    feat: Add navber and update bootstarp@latest

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavberComponent } from './components/navber/navber.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavberComponent
   ],
   imports: [
     BrowserModule,
