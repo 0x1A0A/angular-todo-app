055b0393185da2cb3c5d4b72fa8f6e9cb253db18
0x1A0A <mask.monkey1994@gmail.com>
2023-03-31 15:15:51 +0700

    refactor backend: add response Error to post /user

--- back-end/models/user.js
+++ back-end/models/user.js
@@ -7,6 +7,7 @@ const userSchema = new Schema({
     Name: {
         type: String,
         required: true,
+        unique: true,
         alias: "name",
     },
     FirstName: String,
--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -4,16 +4,31 @@ const model = require('../models/user');
 
 module.exports = {
     create : async function(req,res) {
-        console.log(req.body);
         const user = new model.User( req.body );
+        res.set("Content-Type","application/json");
         
         if (user.validateSync()) {
-            res.send("ERROR");
+            res.send({
+                Error: {
+                    name: "InvalidPayload",
+                }
+            });
+            return;
+        }
+        
+        try {
+            await user.save();
+        } catch (err) {
+            res.send({
+                Error: {
+                    name: err.name,
+                    code: err.code,
+                }
+            });
             return;
         }
 
         res.status(200);
-        res.set("Content-Type","application/json");
         res.send(user.toJSON());
     },
 };
